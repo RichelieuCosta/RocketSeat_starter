f1bd2c2e6e4ee5eb9cb66ee37517956d9285dc05
pedro.lucas <pedro.lucas@lavid.ufpb.br>
2020-09-10 09:52:45 -0300

    app de ToDos com storage

--- JS/app de todos/ToDos.js
+++ JS/app de todos/ToDos.js
@@ -4,28 +4,73 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
+/*[
     'Fazer Café',
     'Estudar JavaScript',
     'Acessar comunidade da Rocketseat'
-];
+];*/
+
+var todos = JSON.parse(localStorage.getItem('list_todos')) || []; // caso não tenha nada no storage, atribui array vazio
 
 function renderTodos(){
+    listElement.innerHTML = '';
+
     for (todo of todos) {
 
-        var todoText = document.createTextNode(todo);
+        var todoText = document.createTextNode(todo+' ');
         var todoElement = document.createElement("li");
         
+        var linkElement = document.createElement('a');
+        var linkText = document.createTextNode('Excluir');
+        linkElement.appendChild(linkText);
+        linkElement.setAttribute('href', '#');
+        var pos = todos.indexOf(todo);
+
+        todoElement.setAttribute('onclick', 'deleteTodo('+ pos +')');
+        
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
         listElement.appendChild(todoElement);
 
     }
 }
 
+renderTodos();
+
 function addTodos() {
 
+    var todo = inputElement.value;
+    console.log(todo)
+    todos.push(todo);
+
+    inputElement.value = '';
     
+    /*
+    var todoText = document.createTextNode(todo);
+    var todoElement = document.createElement("li");
+       
+    todoElement.appendChild(todoText);
+    listElement.appendChild(todoElement);
+    */
+    
+    saveToStorage();
+    renderTodos();
+   
+}
+
+buttonElement.onclick= addTodos;
+
+function deleteTodo(pos){
+
+    todos.splice(pos,1);
+
+    saveToStorage();
+    renderTodos();
 
 }
 
-renderTodos();
+function saveToStorage(){
+
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+
+}
